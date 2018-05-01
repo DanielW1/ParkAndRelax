8d22a94a6878878c3d406e9e66fe113d6ed778ec
Daniel Wasilewski <daniel.wasilewski01@gmail.com>
2018-05-01 21:33:57 +0200

    Initialization commit

---
+++ ParkAndRide-REST/ParkAndRide-REST/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkAndRide_REST.Models;
+
+namespace ParkAndRide_REST.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string[] Index()
+        {
+
+            return new string[] { "ime", "nazwisko" };
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
