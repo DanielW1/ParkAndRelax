4b8bd8b128fe958291a798fa1be2151ac1df9783
Paweł Kowalik <pawerrs@gmail.com>
2018-05-08 15:44:03 +0200

    added service class and installed nuget package

--- MOBILE-APP/Viewer/Viewer/Viewer/ViewModels/ItemsViewModel.cs
+++ MOBILE-APP/Viewer/Viewer/Viewer/ViewModels/ItemsViewModel.cs
@@ -17,7 +17,7 @@ namespace Viewer.ViewModels
 
         public ItemsViewModel()
         {
-            Title = "Browse";
+            Title = "Viewer";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
