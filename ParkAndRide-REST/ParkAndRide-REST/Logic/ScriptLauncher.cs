f7009e56b931c0fe3d29afff21b981cc47bd3a4c
Daniel Wasilewski <daniel.wasilewski01@gmail.com>
2018-05-13 20:11:15 +0200

    Adding class 'scriptLauncher' to run python script and correct model files

---
+++ ParkAndRide-REST/ParkAndRide-REST/Logic/ScriptLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkAndRide_REST.Logic
+{
+    public  class ScriptLauncher
+    {
+        
+        public static void runningScript(string pathToPython,string pathToScript)
+        {
+            //string args = "C:/Users/Daniel/source/repos/SCRIPT/webscraping.py";
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = pathToPython;
+            start.Arguments = string.Format("{0}", pathToScript);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            Process.Start(start);
+        }
+    }
+}
---
+++ ParkAndRide-REST/ParkAndRide-REST/Models/PARK_AND_RELAXContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ParkAndRide_REST.Models
+{
+    public partial class PARK_AND_RELAXContext : DbContext
+    {
+        public virtual DbSet<Event> Event { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=LENOVO-PC\SQLEXPRESS;Database=PARK_AND_RELAX;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.ToTable("EVENT");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Category)
+                    .HasColumnName("CATEGORY")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Date)
+                    .IsRequired()
+                    .HasColumnName("DATE")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Information)
+                    .IsRequired()
+                    .HasColumnName("INFORMATION")
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("NAME")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Place)
+                    .IsRequired()
+                    .HasColumnName("PLACE")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .IsRequired()
+                    .HasColumnName("PRICE")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
